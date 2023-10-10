a308fd37d2d6fd5e4dde3b4329bb234ac30d6c7b
Kerim Keskin <kerim.tsx@gmail.com>
2023-10-11 01:46:12 +0300

    chore: add some icons

---
+++ src/components/ui/icons/index.ts
@@ -0,0 +1,4 @@
+export {default as TradeIcon} from './trade-icon.svg';
+export {default as LogoWhite} from './logo-white.svg';
+export {default as LogoPurple} from './logo-purple.svg';
+export {default as GoogleIcon} from './google-icon.svg';
