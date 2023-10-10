55a5655010825a4bd538ce20979715d09a5034d9
Kerim Keskin <kerim.tsx@gmail.com>
2023-10-11 00:28:53 +0300

    chore: add svgr package

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
