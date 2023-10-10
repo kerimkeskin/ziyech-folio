8248c33428f612e7859a1b6003484e2a28fb6b5f
Kerim Keskin <kerim.tsx@gmail.com>
2023-10-10 23:47:32 +0300

    chore: add session to session provider

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,18 +1,19 @@
 'use client';
 
 import React from 'react';
-import {SessionProvider} from 'next-auth/react';
+import {SessionProvider, SessionProviderProps} from 'next-auth/react';
 import {ToastContainer} from 'react-toastify';
 
 interface IProps {
   children: React.ReactNode;
+  session: SessionProviderProps['session'];
 }
 
 const Providers = (props: IProps) => {
-  const {children} = props;
+  const {children, session} = props;
 
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <ToastContainer autoClose={3000} />
       {children}
     </SessionProvider>
