c38072cb5e91ca6dae590c95f5abe88f14824e6a
Kerim Keskin <kerim.tsx@gmail.com>
2023-10-17 23:40:06 +0300

    chore: update fonts

--- src/styles/font.tsx
+++ src/styles/font.tsx
@@ -3,12 +3,12 @@ import localFont from 'next/font/local';
 export const nexa = localFont({
   src: [
     {
-      path: '../../public/fonts/Nexa-Regular.woff',
+      path: '../../public/fonts/Nexa-Regular.otf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/Nexa-Bold.woff',
+      path: '../../public/fonts/Nexa-Bold.otf',
       weight: '500',
       style: 'normal',
     },
